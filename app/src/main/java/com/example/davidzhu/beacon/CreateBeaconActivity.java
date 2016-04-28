f4dd9b38206a15ad916022c84b4234a67df1acbd
David Zhu <dyzhu12@gmail.com>
2016-04-28 12:29:11 -0400

    Create Beacon UI: Added map for address

--- app/src/main/java/com/example/davidzhu/beacon/CreateBeaconActivity.java
+++ app/src/main/java/com/example/davidzhu/beacon/CreateBeaconActivity.java
@@ -3,14 +3,31 @@ package com.example.davidzhu.beacon;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+
 /**
  * Created by David Zhu on 4/26/2016.
  */
-public class CreateBeaconActivity extends AppCompatActivity {
-
+public class CreateBeaconActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_beacon);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.create_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Disables map toolbar from appearing when marker is clicked
+        UiSettings mUiSettings = mMap.getUiSettings();
+        mUiSettings.setMapToolbarEnabled(false);
     }
 }
