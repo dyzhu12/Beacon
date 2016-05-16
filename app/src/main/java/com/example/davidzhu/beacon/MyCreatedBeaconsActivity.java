00c689f41de879afa80a5d9075a5de853bb7f684
lyoung16 <lyoung16@terpmail.umd.edu>
2016-05-16 15:06:27 -0400

    myCreatedBeacons populated with testArray of beacons, need to implement delete functionality

--- app/src/main/java/com/example/davidzhu/beacon/MyCreatedBeaconsActivity.java
+++ app/src/main/java/com/example/davidzhu/beacon/MyCreatedBeaconsActivity.java
@@ -7,27 +7,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MyCreatedBeaconsActivity extends AppCompatActivity {
     //for testing purposes only. Need to connect to database
     private TextView itemTextView;
-    private String[] testArray = new String[]{
-            "Puppies on the Mall",
-            "Coding Party",
-            "Ultimate Frisbee",
-            "Wow It's My 21st Birthday Party and We're Going to Get Turnt",//test long event titles
-            "Turtle Racing",
-            "Squirrel Watching",
-            "Free Hugs",
-            "Free Pizza",
-            "No Pizza for you",
-            "Google Tech Talk!!",
-            "Ride a camel",
-            "ugh",
-            "test scroll",
-            "are we there yet?"};
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> testArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,24 +30,35 @@ public class MyCreatedBeaconsActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
-        itemTextView = (TextView) findViewById(R.id.beacon_list_item_textview);
 
-        ListView lv = new ListView(this);
-//        lv.setTextFilterEnabled(true);
-        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.beacon_list_item, testArray));
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        testArray = new ArrayList();
+        testArray.add("1. Puppies on the Mall");
+        testArray.add("2. Coding Party");
+        testArray.add("3. Ultimate Frisbee");
+        testArray.add("4. Wow It's My 21st Birthday");
+        testArray.add("5. Turtle Racing");
+        testArray.add("6. Squirrel Watching");
+        testArray.add("7. Free Hugs");
+        testArray.add("8. Free Pizza");
+        testArray.add("9. No Pizza for you");
+        testArray.add("10. Google Tech Talk!!");
+        testArray.add("11. Ride a camel");
+        testArray.add("12. ugh");
+        testArray.add("13. test scroll");
+        testArray.add("14. are we there yet?");
 
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println("Clicked on MySavedBeacon Tag");
-                //make a removeItem method and call it
-            }
-        });
-        setContentView(lv);
 
+        ListView lv = (ListView) findViewById(R.id.list_view_my_created_beacons);
+
+        //TODO replace testArray with database
+        adapter = new ArrayAdapter(this, R.layout.beacon_list_item, R.id.list_item_textview,testArray.toArray());
+        lv.setAdapter(adapter);
     }
 
-    public void onDeleteItemClick(){
-        //delete item from database
-
+    //TODO need to get working with database
+    public void deleteItem(View view){
+        System.out.println("in OnClickDeleteItem, WHOOO!");
+        adapter.notifyDataSetChanged();
     }
+
 }
