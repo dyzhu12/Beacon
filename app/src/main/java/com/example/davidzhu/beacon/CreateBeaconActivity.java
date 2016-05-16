b1fde2749dafa812fa1c61e10cc7ddd2aa809ffa
David Zhu <dyzhu12@gmail.com>
2016-05-15 23:58:51 -0400

    Implemented dialog - next steps are to load the photo

--- app/src/main/java/com/example/davidzhu/beacon/CreateBeaconActivity.java
+++ app/src/main/java/com/example/davidzhu/beacon/CreateBeaconActivity.java
@@ -1,12 +1,23 @@
 package com.example.davidzhu.beacon;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MotionEvent;
@@ -14,6 +25,8 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TimePicker;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +34,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -150,6 +169,91 @@ public class CreateBeaconActivity extends AppCompatActivity implements OnMapRead
 
         String timeText = dateFormat.format(beaconDate) + " from  " + startTime + " to " + endTime;
         dateText.setText(timeText);
+    }
+
+    /*
+     * Handler for ImageButton for adding photos
+     */
+    public void addPhoto(final View v) {
+
+        final CharSequence[] items = { "Take Photo", "Choose Photo" };
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        dialogBuilder.setTitle(R.string.change_photo);
+        dialogBuilder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    takePhoto(v);
+                } else if (items[item].equals("Choose Photo")) {
+                    choosePhoto(v);
+                }
+            }
+        });
+        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {}
+        });
+
+        dialogBuilder.show();
+    }
+
+    public void takePhoto(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+    public void choosePhoto(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        ImageButton imageButton = (ImageButton) findViewById(R.id.add_photo_button);
+//        switch(requestCode) {
+//            case 0:
+//                if (resultCode == RESULT_OK) {
+//
+//                }
+//                break;
+//            case 1:
+//                if (resultCode == RESULT_OK) {
+//
+//                }
+//                break;
+//            default: break;
+//        }
+        if (resultCode == RESULT_OK) {
+            System.out.println("RESULT OK");
+            System.out.println(requestCode);
+            System.out.println(resultCode);
+            System.out.println(imageReturnedIntent);
+            System.out.println(imageReturnedIntent.getDataString());
+
+            Uri uri = imageReturnedIntent.getData();
+            imageButton.setImageDrawable(null);
+            imageButton.setBackgroundColor(Color.TRANSPARENT);
+
+            System.out.println(getImageBitmap(uri.getPath()));
+            imageButton.setImageBitmap(getImageBitmap(imageReturnedIntent.getDataString()));
+
+        }
+    }
+
+    private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+        }
+        return bm;
     }
 
 }
